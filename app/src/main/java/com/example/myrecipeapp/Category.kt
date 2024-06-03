9dee368b6c297204438f17e35fefc7d130d9a57c
Imran Ahmad <imranjmi463@gmail.com>
2024-06-03 21:04:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -0,0 +1,19 @@
+package com.example.myrecipeapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+//making parcelable is neccessary to tranfer an object from one screen to another screen
+//in parcelization there are two thing serialization and deserialization
+//serialization -> to convert object into string so that it can be transferred from one screen to another
+//deserialization -> to convert string into object to use its property
+@Parcelize
+data class Category(
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+):Parcelable
+
+
+data class CategoriesResponse(val categories: List<Category>)
---
+++ app/src/main/java/com/example/myrecipeapp/RecipeApp.kt
@@ -0,0 +1,33 @@
+package com.example.myrecipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun RecipeApp(navController: NavHostController){
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+
+
+    NavHost(navController = navController, startDestination = Screen.RecipeScreen.route){
+        composable(route = Screen.RecipeScreen.route){
+            recipeScreen(viewState = viewState, navigateToDetail = {
+                //this part is reesponsible for passing data from current screen to detail screen
+                //it ustilizes the savedStateHandle, which is component of compose navigation framework
+
+                navController.currentBackStackEntry?.savedStateHandle?.set("cat",it)
+                navController.navigate(Screen.DetailScreen.route)
+            })
+        }
+
+        composable(route = Screen.DetailScreen.route){
+            val category = navController.previousBackStackEntry?.savedStateHandle?.
+                    get<Category>("cat")?: Category("","","","")
+            CategoryDetailScreen(category = category)
+        }
+    }
+}
